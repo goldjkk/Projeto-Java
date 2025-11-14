de0ba240342e33d2409c617d7087025d11d7092e
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2025-11-14 09:03:52 -0300

    Update UsuarioDAO.java

--- FEIFOOD/dao/UsuarioDAO.java
+++ FEIFOOD/dao/UsuarioDAO.java
@@ -1,1 +1,9 @@
-package dao; import model.Usuario; import java.util.Optional; public interface UsuarioDAO { void salvar(Usuario u); Optional<Usuario> buscarPorEmail(String email); }
+package dao;
+
+import model.Usuario;
+import java.util.Optional;
+
+public interface UsuarioDAO {
+    void salvar(Usuario u);
+    Optional<Usuario> buscarPorEmail(String email);
+}
