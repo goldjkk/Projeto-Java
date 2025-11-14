772170b29968203e3322f75ce193a33b17070190
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2025-11-14 09:05:55 -0300

    Update PedidoItem.java

--- FEIFOOD/model/PedidoItem.java
+++ FEIFOOD/model/PedidoItem.java
@@ -1,1 +1,39 @@
-package model; public class PedidoItem { private Alimento alimento; private int quantidade; private double precoUnitario; public PedidoItem(){} public PedidoItem(Alimento a,int q,double p){this.alimento=a;this.quantidade=q;this.precoUnitario=p;} public Alimento getAlimento(){return alimento;} public void setAlimento(Alimento a){this.alimento=a;} public int getQuantidade(){return quantidade;} public void setQuantidade(int q){this.quantidade=q;} public double getPrecoUnitario(){return precoUnitario;} public void setPrecoUnitario(double p){this.precoUnitario=p;} }
+package model;
+
+public class PedidoItem {
+    private Alimento alimento;
+    private int quantidade;
+    private double precoUnitario;
+
+    public PedidoItem() {}
+
+    public PedidoItem(Alimento alimento, int quantidade, double precoUnitario) {
+        this.alimento = alimento;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Alimento getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(Alimento alimento) {
+        this.alimento = alimento;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
