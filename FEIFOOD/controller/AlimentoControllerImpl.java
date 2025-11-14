18adfa705f2ccbb4365f379c906a68b6bd4d0087
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2025-11-14 09:02:28 -0300

    Update AlimentoControllerImpl.java

--- FEIFOOD/controller/AlimentoControllerImpl.java
+++ FEIFOOD/controller/AlimentoControllerImpl.java
@@ -1,1 +1,18 @@
-package controller; import dao.AlimentoDAO; import model.Alimento; import java.util.*; public class AlimentoControllerImpl implements AlimentoController { private final AlimentoDAO alimentoDAO; public AlimentoControllerImpl(AlimentoDAO d){this.alimentoDAO=d;} public List<Alimento> buscarPorNome(String termo,int limit,int offset){ return alimentoDAO.buscarPorNome(termo,limit,offset);} }
+package controller;
+
+import dao.AlimentoDAO;
+import model.Alimento;
+import java.util.List;
+
+public class AlimentoControllerImpl implements AlimentoController {
+    private final AlimentoDAO alimentoDAO;
+
+    public AlimentoControllerImpl(AlimentoDAO dao) {
+        this.alimentoDAO = dao;
+    }
+
+    @Override
+    public List<Alimento> buscarPorNome(String termo, int limit, int offset) {
+        return alimentoDAO.buscarPorNome(termo, limit, offset);
+    }
+}
